79860d4a44508e4ca160cd7f0484917831183354
Larson <rlrson@uiowa.edu>
2017-10-03 13:25:51 -0500

    Update dropdown documentation.

--- src/components/toolbar/dropdown.js
+++ src/components/toolbar/dropdown.js
@@ -7,6 +7,12 @@ import 'react-virtualized/styles.css'
 import 'react-virtualized-select/styles.css'
 
 export default class extends React.Component {
+
+  /**
+    * @constructor
+    * @description Creates a dropdown object which can be used to search or select.
+    * @returns React-select component ready to be populated.
+    */
   constructor() {
     super();
     this.state = {
@@ -15,21 +21,42 @@ export default class extends React.Component {
       placeholder: 'Select...'
     };
 
-    // Bind Functions
+    // Bind Functions to React Component
     this.handleChange = this.handleChange.bind(this);
     this.filterOptions = this.filterOptions.bind(this);
   }
 
+  /**
+    * @method
+    * @description Temporary method to change the value of the dropdown.
+    * @param {event} - Event triggered by dropdown changing value.
+    * @this This refers to the react-select dropdown object.
+    * @returns A newly set state reflecting the changed value.
+    */
   handleChange(event) {
     console.log(event.value);
     if (event !== null) this.setState({value: event.value});
   }
 
+  /**
+    * @method
+    * @description Creates an array of options for react-select-fast-filter-options,
+    * so the dropdown can have a faster lookup.
+    * @param {array} options - Our state options.
+    * @returns Options for our dropdown's fast-select.
+    */
   filterOptions() {
     const option = this.state.options;
     return createFilterOptions(option);
   }
 
+  /**
+    * @method
+    * @description Method permitting Component to update. Only updates if the props
+    * contain our data. Otherwise we just have empty dropdowns.
+    * @this this refers to our dropdown component.
+    * @returns Sets our component state to reflect our incoming data.
+    */
   componentWillReceiveProps(nextProps) {
       if (nextProps.data !== undefined) {
           return this.setState({
@@ -39,15 +66,21 @@ export default class extends React.Component {
       }
   }
 
+  /**
+    * @method
+    * @description Renders the Dropwdown component.
+    * @returns Div containing only the dropdown and it's options.
+    */
   render() {
-    
     return (
       <Select
         name="form-field-name"
+        options={this.state.options}
+
         value={this.state.value}  //"PSY:1001"
         //resetValue="one"
-        options={this.state.options}
         placeholder={this.state.placeholder}
+
         onChange={this.props.callback}
         //onChange={this.handleChange}
       />
