068b1539a5b897175480355104f9384a6ae9d886
Larson <rlrson@uiowa.edu>
2017-10-03 14:28:59 -0500

    Added toolbar to components, got rid of huge chunk of code in our main App's render().

--- src/components/toolbar/toolbar.js
+++ src/components/toolbar/toolbar.js
@@ -1,35 +1,33 @@
 import React from 'react';
-import dropdown from './dropdown';
+import Dropdown from './dropdown';
 
 export default class extends React.Component {
   render() {
     return (
       <div className="row">
+        <div className="col-xs-3">
+          <Dropdown
+            data={this.props.major_dropdown_options}
+            placeholder={"Major"}
+            callback={this.props.majorCallback}
+          />
+        </div>
 
-          <div className="col-xs-3">
-            <dropdown 
-              data={this.props.major_dropdown_options} 
-              placeholder={"Major"}
-              callback={this.props.majorCallback}
-            />
-          </div>
+        <div className="col-xs-6">
+          <Dropdown
+            data={this.props.course_dropdown_options}
+            placeholder={"Course"}
+            callback={this.props.courseCallback}
+          />
+        </div>
 
-          <div className="col-xs-6">
-            <dropdown 
-              data={this.props.course_dropdown_options} 
-              placeholder={"Course"}
-              callback={this.props.courseCallback}
-            />
-          </div>
-
-          <div className="col-xs-3">
-            <dropdown 
-              data={this.props.department_dropdown_options} 
-              placeholder={"Department"}
-              callback={this.props.departmentCallback}
-            />
-          </div>
-
+        <div className="col-xs-3">
+          <Dropdown
+            data={this.props.department_dropdown_options}
+            placeholder={"Department"}
+            callback={this.props.departmentCallback}
+          />
+        </div>
       </div>
     );
   }
