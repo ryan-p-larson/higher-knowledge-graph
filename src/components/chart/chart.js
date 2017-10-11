72ec4cf9f3c104dd003974e66ee92b89f92c201c
Larson <rlrson@uiowa.edu>
2017-10-11 12:44:11 -0500

    Added documentation to chart module.

--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -4,8 +4,17 @@ import Prompt   from './prompts';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
+/**
+  * @class
+  * @description React Component in charge of handling the chart display.
+  * @param {string} view - Describes the type of view the app is in.
+  * @param {string} active - Formatted label of the active data subset.
+  * @param {array} nodes - Array of course objects (can be empty)
+  * @param {array} links - Array of objects linking courses (can be empty)
+  * @param {function} callback - Callback for each view, creating graph for each.
+  * @returns Either a chart if data is valid, else a prompt of charts to choose.
+  */
 export default class extends React.Component {
-
   render() {
     return (
         <div className="row">
